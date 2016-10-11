b74f37520a889d9f548b4106d6e784c6eb29aa93
Jefferson Viana Fonseca Abreu <jeffvfa@hotmail.com>
2016-10-11 20:08:10 +0000

    renomeando  a pasta de arquivos do programa

---
+++ source/teste_server.py
@@ -0,0 +1,30 @@
+import socket
+import thread
+
+HOST = '172.17.32.193'      # Endereco IP do Servidor
+PORT = 12001  
+
+def conectado(con, cliente):
+    print 'Conectado por', cliente
+
+    while True:
+        msg = con.recv(1024)
+        if not msg: break
+        print cliente, msg
+
+    print 'Finalizando conexao do cliente', cliente
+    con.close()
+    thread.exit()
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+orig = (HOST, PORT)
+
+tcp.bind(orig)
+tcp.listen(1)
+
+while True:
+    con, cliente = tcp.accept()
+    thread.start_new_thread(conectado, tuple([con, cliente]))
+
+tcp.close()
